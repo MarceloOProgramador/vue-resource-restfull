1aa918d52046d848c32b8f5354bac98ec8372b27
Marcelo Pereira <marcelooprogramador@gmail.com>
2018-09-18 22:05:45 -0300

    Configuring ambient

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from "vue-resource"
+import VueRouter from "vue-router"
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
+Vue.use(VueResource);
+Vue.use(VueRouter);
 
 new Vue({
   el: '#app',
